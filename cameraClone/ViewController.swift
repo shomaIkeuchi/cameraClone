ae11714a1e2d070da217b4724ae769f776debddf
shoma <shoma.aptx4869@gmail.com>
2019-07-21 03:46:17 +0900

    これでどうだ

--- cameraClone/ViewController.swift
+++ cameraClone/ViewController.swift
@@ -51,23 +51,24 @@ class ViewController: UIViewController , UINavigationControllerDelegate , UIImag
         
            }
     
+}
+
+    @IBAction func SNSButtonAction(_ sender: Any) {
+        
         func SNSButtonAction(_ sender: Any) {
             if let shareImage = pictureImage.image {
-               let shareItems = [shareImage]
-                let controller = UIActivityViewController(activityItems: shareItems ,
-                                                          applicationActivities: nil)
+                let shareItems = [shareImage]
+                let controller = UIActivityViewController(activityItems: shareItems ,applicationActivities: nil)
                 controller.popoverPresentationController?.sourceView = view
                 present(controller, animated: true, completion: nil)
             }
             
-    }
+        }
         
         func imagePickerController(_ picker: UIImagePickerController , didFinishPickingMediaWithInfo
             info: [UIImagePickerController.InfoKey : Any]){
             pictureImage.image = info[UIImagePickerController . InfoKey.originalImage] as? UIImage
             dismiss(animated: true, completion: nil)
         }
-}
-
-
+    }
 }
